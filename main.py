3a8f3cd1bebd9ba7e09e5dd5e8a47e66ab2f6819
Kevin Tao <kevintao841@yahoo.co.uk>
2021-11-08 12:14:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import emeka.name
-import will.will
-import kevin.kevin
-import callum.name
+import emeka
+import will
+import kevin
+import callum
 
-print(emeka.name)
-print(will.will)
-print(kevin.kevin)
-print(callum.name)
+print(emeka.name())
+print(will.will())
+print(kevin.kevin())
+print(callum.name())
