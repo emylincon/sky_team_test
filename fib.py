8bb5052284506ecaeaf9aba451fbec11a367746d
callumholmes-sky <callum.holmes@sky.uk>
2021-11-08 15:31:21 +0000

    Added print function to fib.py

--- fib.py
+++ fib.py
@@ -1,4 +1,10 @@
 fib = lambda x: fib(x-2) + fib(x-1) if x >=2 else x
 
-print(fib(10))
+limit = input("How many fibonacci numbers would you like to print? ")
+try:
+    limit = int(limit)
+except ValueError:
+    print("Please enter an integer")
 
+for i in range(limit):
+    print(fib(i))
